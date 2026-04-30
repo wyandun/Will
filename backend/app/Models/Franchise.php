f4586448494e9dbe786aac2df653117d6f5f06bd
Nicolas Garcia <angarcia1@espe.edu.ec>
2026-04-30 18:30:38 -0500

    style: fix formatting issues with Laravel Pint

--- backend/app/Models/Franchise.php
+++ backend/app/Models/Franchise.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
 /**
  * @property string $name
  */
